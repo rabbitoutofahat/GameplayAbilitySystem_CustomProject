86aef8c5e497e4f314895c51f7643c837b0bab16
rabbitoutofahat <72710404+rabbitoutofahat@users.noreply.github.com>
2025-08-26 10:39:33 +0100

    Attribute Menu Widget Controller

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,12 @@
+// Copyright Druid Mechanics
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+}
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+}

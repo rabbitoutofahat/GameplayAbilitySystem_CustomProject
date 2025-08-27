9a45588ca4b174621e54b140c67617e94df6cee7
rabbitoutofahat <72710404+rabbitoutofahat@users.noreply.github.com>
2025-08-27 15:14:29 +0100

    Aura Input Component

---
+++ Source/Aura/Public/Input/AuraInputComponent.h
@@ -0,0 +1,23 @@
+// Copyright Druid Mechanics
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnhancedInputComponent.h"
+#include "AuraInputConfig.h"
+#include "AuraInputComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraInputComponent : public UEnhancedInputComponent
+{
+	GENERATED_BODY()
+	
+public:
+	// (We'll be calling BindAction() in this function so we need a User Object)
+	template<class UserClass, typename PressedFuncType, typename ReleasedFuncType, typename HeldFuncType>
+	void BindAbilityActions(const UAuraInputConfig* InputConfig, UserClass* Object, PressedFuncType PressedFunc, ReleasedFuncType ReleasedFunc, HeldFuncType HeldFunc);
+};
+

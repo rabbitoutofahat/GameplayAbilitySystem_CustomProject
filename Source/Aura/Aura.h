6f693043151efee7d5352ee010987c9c81d7f520
rabbitoutofahat <72710404+rabbitoutofahat@users.noreply.github.com>
2025-09-01 11:05:57 +0100

    Projectile Collision Channel

--- Source/Aura/Aura.h
+++ Source/Aura/Aura.h
@@ -7,3 +7,5 @@
 // The value 250 is associated with the colour red for our custom stencil used for highlighting enemies
 #define CUSTOM_DEPTH_RED 250
 
+#define ECC_Projectile ECollisionChannel::ECC_GameTraceChannel1
+

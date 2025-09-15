ffbd2ba38449b26e8e54fe0fd7ed8ab3e95d7304
rabbitoutofahat <72710404+rabbitoutofahat@users.noreply.github.com>
2025-09-15 13:49:23 +0100

    Combat Target

--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -35,6 +35,8 @@ public:
 	/* Combat Interface */
 	virtual int32 GetPlayerLevel() override;
 	virtual void Die() override;
+	virtual void SetCombatTarget_Implementation(AActor* InCombatTarget) override;
+	virtual AActor* GetCombatTarget_Implementation() const override;
 	/* end Combat Interface */
 
 	UPROPERTY(BlueprintAssignable)
@@ -53,6 +55,9 @@ public:
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Combat")
 	float LifeSpan = 5.f;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Combat")
+	TObjectPtr<AActor> CombatTarget;
 
 protected:
 	virtual void BeginPlay() override;
--- Source/Aura/Public/Interaction/EnemyInterface.h
+++ Source/Aura/Public/Interaction/EnemyInterface.h
@@ -22,11 +22,16 @@ class AURA_API IEnemyInterface
 
 	/* 
 	* Add interface functions to this class.This is the class that will be inherited to implement this interface.
-	* Rather than casting to AuraEnemy and calling a function on that class, we have more flexibility with an interface, since any class can implement it and provide its own
-	* functionality in the HighlightActor() and UnhighlightActor() functions.
+	* Rather than casting to AuraEnemy and calling a function on that class, we have more flexibility with an interface, since any class can implement it and provide its own functionality.
 	*/
 
 public:
 	virtual void HighlightActor() = 0; // By adding '=0' we make this a 'pure virtual function' (or 'abstract'), meaning we do not need to provide a definition
 	virtual void UnHighlightActor() = 0;
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent) // BlueprintNativeEvents don't require a virtual keyword, instead you override it's Implementation on the desired class (in this case AAuraEnemy)
+	void SetCombatTarget(AActor* InCombatTarget);
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	AActor* GetCombatTarget() const;
 };

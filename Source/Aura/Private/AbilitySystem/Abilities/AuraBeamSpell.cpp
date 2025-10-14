54c475ea5b79393ddd38165325a1c8d9350436a9
rabbitoutofahat <72710404+rabbitoutofahat@users.noreply.github.com>
2025-10-14 16:45:09 +0100

    Aura Beam Spell

---
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraBeamSpell.cpp
@@ -0,0 +1,25 @@
+// Copyright Druid Mechanics
+
+
+#include "AbilitySystem/Abilities/AuraBeamSpell.h"
+
+void UAuraBeamSpell::StoreMouseDataInfo(const FHitResult& HitResult)
+{
+	if (HitResult.bBlockingHit)
+	{
+		MouseHitLocation = HitResult.ImpactPoint;
+		MouseHitActor = HitResult.GetActor();
+	}
+	else
+	{
+		CancelAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true); // GameplayAbility.h has a lot of 'current' info at the ready for instanced abilities
+	}
+}
+
+void UAuraBeamSpell::StoreOwnerPlayerController()
+{
+	if (CurrentActorInfo)
+	{
+		OwnerPlayerController = CurrentActorInfo->PlayerController.Get();
+	}
+}

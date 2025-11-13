ec78e1d3a5d53e1c054bc9bafab24a271816a37e
rabbitoutofahat <72710404+rabbitoutofahat@users.noreply.github.com>
2025-11-13 11:32:40 +0000

    Different Highlight Colours

--- Source/Aura/Aura.h
+++ Source/Aura/Aura.h
@@ -6,6 +6,8 @@
 
 // The value 250 is associated with the colour red for our custom stencil used for highlighting enemies
 #define CUSTOM_DEPTH_RED 250
+#define CUSTOM_DEPTH_BLUE 251
+#define CUSTOM_DEPTH_TAN 252
 
 #define ECC_Projectile ECollisionChannel::ECC_GameTraceChannel1
 #define ECC_Target ECollisionChannel::ECC_GameTraceChannel2

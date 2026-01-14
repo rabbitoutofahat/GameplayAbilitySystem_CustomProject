3a22f4e650194e44335d55cd27426dce4eb98dca
rabbitoutofahat <72710404+rabbitoutofahat@users.noreply.github.com>
2026-01-14 13:44:43 +0000

    Randomised Rift Spawn Location

--- Source/Aura/Private/AbilitySystem/Abilities/Vessel/DemonfireRift.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/Vessel/DemonfireRift.cpp
@@ -3,3 +3,15 @@
 
 #include "AbilitySystem/Abilities/Vessel/DemonfireRift.h"
 
+FVector UDemonfireRift::GetRandomRiftSpawnLocation() const
+{
+	const FVector Location = GetAvatarActorFromActorInfo()->GetActorLocation();
+	const FVector Forward = GetAvatarActorFromActorInfo()->GetActorForwardVector();
+	const FVector Left = Forward.RotateAngleAxis(-90.f, FVector::UpVector);
+	const float RandomSpread = FMath::RandRange(0.f, 180.f);
+
+	const FVector SpawnDirection = Left.RotateAngleAxis(RandomSpread, FVector::UpVector);
+	FVector SpawnLocation = Location + SpawnDirection * FMath::FRandRange(RiftSpawnDistanceMin, RiftSpawnDistanceMax);
+
+	return SpawnLocation;
+}
--- Source/Aura/Public/AbilitySystem/Abilities/Vessel/DemonfireRift.h
+++ Source/Aura/Public/AbilitySystem/Abilities/Vessel/DemonfireRift.h
@@ -6,6 +6,8 @@
 #include "AbilitySystem/Abilities/AuraDamageGameplayAbility.h"
 #include "DemonfireRift.generated.h"
 
+class AAuraProjectile;
+
 /**
  * 
  */
@@ -15,9 +17,21 @@ class AURA_API UDemonfireRift : public UAuraDamageGameplayAbility
 	GENERATED_BODY()
 	
 protected:
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
-	int32 NumProjectiles;
+	UFUNCTION(BlueprintCallable)
+	FVector GetRandomRiftSpawnLocation() const;
+
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<AAuraProjectile> RiftProjectileClass;
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
-	float RiftDuration;
+	int32 NumProjectiles = 12;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	float RiftDuration = 8.f;
+
+	UPROPERTY(EditDefaultsOnly)
+	float RiftSpawnDistanceMin = 150.f;
+
+	UPROPERTY(EditDefaultsOnly)
+	float RiftSpawnDistanceMax = 400.f;
 };
